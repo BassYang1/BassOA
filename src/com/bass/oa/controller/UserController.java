be155ab46105bc4df9a23b3bfcae5a1fed87679c
BassYang1 <yjdaio@163.com>
2017-10-25 18:07:47 +0800

    国际化

--- src/com/bass/oa/controller/UserController.java
+++ src/com/bass/oa/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.bass.oa.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContext;
 
 import com.bass.oa.model.Result;
 import com.bass.oa.model.UserModel;
@@ -10,6 +13,14 @@ import com.bass.oa.model.UserModel;
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
+	@RequestMapping(value = "/test")
+	public ModelAndView test(HttpServletRequest request) {
+		RequestContext requestContext = new RequestContext(request);
+		Result result = new Result(false);
+		result.setMessage(requestContext.getMessage("app.name"));
+		return new ModelAndView("login", "result", result);
+	}
+	
 	@RequestMapping(value = "/login")
 	public String login() {
 		return "login";
